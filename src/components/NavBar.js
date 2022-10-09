07144d4f3a406f3ae62e657fc15ea4a53bca8296
Rob Cole <rob-cole@hotmail.co.uk>
2022-10-09 17:23:08 +0000

    Fix issue with navbar not auto collapsing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from '../css/NavBar.module.css';
 import { NavLink } from "react-router-dom";
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Navbar className={styles.NavBar} expand="lg">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="lg" variant="dark">
             <Container fluid>
-                <Navbar.Brand><img src={logo} /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Brand><img src={logo} alt="logo" /></Navbar.Brand>
+                <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-home"></i>Home</NavLink>
-                        <NavLink to="/servers" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-headset"></i>Servers</NavLink>
-                        <NavLink to="/tutorials" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-book"></i>Tutorials / Guides</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} exact to="/" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-home"></i>Home</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/servers" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-headset"></i>Servers</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/tutorials" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-book"></i>Tutorials / Guides</NavLink>
                     </Nav>
                     <Nav>
-                        <NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-id-card"></i>Profile</NavLink>
-                        <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-sign-in-alt"></i>Login / Signup</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/profile" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-id-card"></i>Profile</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/signin" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-sign-in-alt"></i>Login / Signup</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
