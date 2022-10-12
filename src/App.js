07e35693cd793ac1ed8718d470ba8df2dada2eb6
Rob Cole <rob-cole@hotmail.co.uk>
2022-10-12 20:55:25 +0000

    Add Signin Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           <Route exact path='/servers' render={() => <h1>Servers</h1>} />
           <Route exact path='/tutorials' render={() => <h1>Tutorials</h1>} />
           <Route exact path='/profile' render={() => <h1>Profile</h1>} />
-          <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
+          <Route exact path='/signin' render={() => <SignInForm />} />
           <Route exact path='/signup' render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import { Form, Button, Col, Row, Container, Alert, Image } from "react-bootstrap";
+
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../css/SignInUpForm.module.css";
+import appStyles from "../../App.module.css";
+import image from "../../assets/signin.jpg";
+
+function SignInForm() {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                className={styles.Input}
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                className={styles.Input}
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Button
+              type="submit"
+            >
+              Sign in
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={image}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+export default SignInForm;
