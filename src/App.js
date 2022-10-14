8b286cbf81b43630dc7075337db0392d0d78d4eb
Rob Cole <rob-cole@hotmail.co.uk>
2022-10-14 22:21:42 +0000

    Modify burger menu collapse logic

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
           <Route exact path='/profile' render={() => <h1>Profile</h1>} />
           <Route exact path='/signin' render={() => <SignInForm />} />
           <Route exact path='/signup' render={() => <SignUpForm />} />
+          <Route exact path='/servers/create' render={() => <h1>Add Server</h1>} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from '../css/NavBar.module.css';
 import { NavLink } from "react-router-dom";
 import logo from '../assets/logo.png';
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
 
-    const loggedInIcons = 
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post("dj-rest-auth/logout/");
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const addServerIcon = (
         <>
-            <NavLink onClick={() => setExpanded(false)} to="/profile" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-id-card"></i>{currentUser?.username}</NavLink>
+        <NavLink to={'/servers/create'} className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-plus-square"></i>Add Server</NavLink>
+        </>
+    )
+
+    const loggedInIcons =
+        <>
+            <NavLink to={`/profiles/${currentUser?.profile_id}`} className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-id-card"></i>{currentUser?.username}</NavLink>
+            <NavLink onClick={handleSignOut} to="/" className={styles.NavLink}><i className="fas fa-sign-out-alt"></i>Sign Out</NavLink>
         </>
 
-    const loggedOutIcons = 
-        <> 
-            <NavLink onClick={() => setExpanded(false)} to="/signin" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-sign-in-alt"></i>Sign In</NavLink>
-            <NavLink onClick={() => setExpanded(false)} to="/signup" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-sign-in-alt"></i>Signup</NavLink>
+    const loggedOutIcons =
+        <>
+            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-sign-in-alt"></i>Sign In</NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-sign-in-alt"></i>Signup</NavLink>
         </>
-
-
-    const [expanded, setExpanded] = useState(false);
 
     return (
         <Navbar expanded={expanded} className={styles.NavBar} expand="lg" variant="dark">
             <Container fluid>
                 <Navbar.Brand><img src={logo} alt="logo" /></Navbar.Brand>
-                <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <NavLink onClick={() => setExpanded(false)} exact to="/" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-home"></i>Home</NavLink>
-                        <NavLink onClick={() => setExpanded(false)} to="/servers" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-headset"></i>Servers</NavLink>
-                        <NavLink onClick={() => setExpanded(false)} to="/tutorials" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-book"></i>Tutorials / Guides</NavLink>
+                        <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-home"></i>Home</NavLink>
+                        <NavLink exact to="/servers" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-headset"></i>Servers</NavLink>
+                        <NavLink to="/tutorials" className={styles.NavLink} activeClassName={styles.Active}><i className="fas fa-book"></i>Tutorials / Guides</NavLink>
                     </Nav>
                     <Nav>
+                        {currentUser && addServerIcon}
                         {currentUser ? loggedInIcons : loggedOutIcons}
                     </Nav>
                 </Navbar.Collapse>
