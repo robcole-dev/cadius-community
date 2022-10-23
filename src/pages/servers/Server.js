b103d437306c395d984e81d1ae61224501b6edc2
Rob Cole <rob-cole@hotmail.co.uk>
2022-10-23 09:27:42 +0000

    Remove rate this server section from Server Detail

--- src/pages/servers/Server.js
+++ src/pages/servers/Server.js
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import styles from "../../css/Server.module.css";
-import { Rating } from "@mui/material";
+import { Rating } from 'react-simple-star-rating';
 
 const Server = (props) => {
     const {
-        id,
         author,
         server_address,
         server_name,
         game,
         banner,
-        created_date,
         avg_rating,
         author_id,
         author_image,
+        serverPage,
     } = props;
 
     const currentUser = useCurrentUser();
     const is_author = currentUser?.username === author
 
-    let gameTitle
-    if ({ game } === 'se') {
+    let gameTitle;
+    console.log(game)
+    if (game === 'se') {
         gameTitle = "Space Engineers"
-    } else if ({ game } === 'mc') {
+    } else if ( game  === 'mc') {
         gameTitle = "Minecraft"
     } else {
         gameTitle = ""
@@ -37,13 +37,20 @@ const Server = (props) => {
             <Card.Body>
                 <Card.Title>{server_name}</Card.Title>
                 <hr className={styles.hr} />
+                <Card.Text>{gameTitle}</Card.Text>
+                <hr className={styles.hr} />
                 <Card.Text>{server_address}</Card.Text>
                 <hr className={styles.hr} />
-                <Rating precision={0.5} value={avg_rating} onChange={(event, newValue) => {avg_rating(newValue)}} />
+                <Rating initialValue={avg_rating} readonly={true} />
                 <hr className={styles.hr} />
-                <Card.Text>Server Owner: <Link to={`/profiles/${author_id}`}>{author}</Link></Card.Text>
+                <Card.Text>Server Owner: <Link to={`/profiles/${author_id}`}>{author_image}{author}</Link></Card.Text>
             </Card.Body>
+            {is_author && serverPage && <>
+                <hr className={styles.hr} />
+                Edit Delete</>}
         </Card>
+
+
     )
 }
 
--- src/pages/servers/ServerPage.js
+++ src/pages/servers/ServerPage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { Container } from "react-bootstrap";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Server from "./Server";
@@ -15,9 +14,9 @@ function ServerPage() {
         const handleMount = async () => {
             try {
                 const [{data: server}] = await Promise.all([
-                    axiosReq.get(`/servers/${id}`)
+                    axiosReq.get(`/servers/${id}`),
                 ])
-                setServer({results: [server]})
+                setServer({results: [server]});
                 console.log(server)
             } catch(err){
                 console.log(err)
@@ -29,7 +28,7 @@ function ServerPage() {
     return (
         <Row>
             <Col>
-                <Server {...server.results[0]} setServers={setServer} />
+                <Server {...server.results[0]} setServers={setServer} serverPage/>
             </Col>
         </Row>
     );
