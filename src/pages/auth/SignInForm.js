6d2a6088ba51f40fef56e90838532d5af23193cd
Rob Cole <rob-cole@hotmail.co.uk>
2022-10-29 04:12:37 +0000

    Remove TokenTimestamp from signin

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -29,7 +29,7 @@ function SignInForm() {
     try {
       const {data} = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      setTokenTimestamp(data);
+      
       history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
